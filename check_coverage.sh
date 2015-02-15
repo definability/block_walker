aa21a430266678a08f54e6b770f532f850b7f90b
char-lie <webmaster976@gmail.com>
2015-02-15 20:19:15 +0200

    Coverage checker added

---
+++ check_coverage.sh
@@ -0,0 +1,8 @@
+coverage run -m tests.classes.point
+mv .coverage .coverage.point
+coverage run -m tests.classes.game_object
+mv .coverage .coverage.game_object
+coverage run -m tests.classes.singleton
+mv .coverage .coverage.singleton
+coverage combine
+coverage html
