aab6c47acd1de755e9f3973b92e0ed972fb2c694
char-lie <webmaster976@gmail.com>
2015-02-16 19:41:26 +0200

    Restructured the project

--- check_coverage.sh
+++ check_coverage.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-coverage run -m tests.classes.point
+coverage run -m tests.classes.game.point
 mv .coverage .coverage.point
-coverage run -m tests.classes.game_object
-mv .coverage .coverage.game_object
-coverage run -m tests.classes.singleton
+coverage run -m tests.classes.util.singleton
 mv .coverage .coverage.singleton
-coverage run -m tests.classes.abstract_drawer
+coverage run -m tests.classes.graphics.drawer
 mv .coverage .coverage.abstract_drawer
+coverage run -m tests.classes.game.game_object
+mv .coverage .coverage.game_object
 coverage combine
 #coverage html
 coverage report --show-missing
--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-python -m unittest tests.classes.point
+python -m unittest tests.classes.game.point
+python -m unittest tests.classes.util.singleton
+python -m unittest tests.classes.graphics.drawer
+python -m unittest tests.classes.game.game_object
